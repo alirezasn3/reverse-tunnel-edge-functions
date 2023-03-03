4b6c0b31d41be25475559bf207d64f30fd5932fc
Alireza Saadatnia <alirezasn3@gmail.com>
2023-03-04 01:44:01 +0330

    testing

--- netlify/edge-functions/index/index.js
+++ netlify/edge-functions/index/index.js
@@ -1,12 +1,12 @@
 export default async (request, context) => {
   try {
     const { pathname } = new URL(request.url)
-    const [serverIP,clientPort] = pathname.split('-')
+    const [serverIP, clientPort] = pathname.split('-')
     const res = await fetch(`http://${serverIP}.nip.io/${context.ip}-${clientPort}`)
     const port = await res.text()
     return new Response(port)
   } catch (error) {
     console.log(error)
-    return new Response(null)
+    return new Response(context.ip)
   }
 }
