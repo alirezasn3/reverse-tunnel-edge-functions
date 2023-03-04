a57fb97fb3b16690dd98e8b2e98b86b6b74f0890
Alireza Saadatnia <alirezasn3@gmail.com>
2023-03-04 19:17:42 +0330

    testing

--- netlify/edge-functions/index/index.js
+++ netlify/edge-functions/index/index.js
@@ -2,8 +2,11 @@ export default async (request, context) => {
   try {
     let { pathname } = new URL(request.url)
     if (pathname.includes('/confirmation')) {
+      console.log(pathname)
       pathname = pathname.replace('/confirmation', '')
+      console.log(pathname)
       const [serverIP, clientIpAndPort] = pathname.split('-')
+      console.log(serverIP, clientIpAndPort)
       const res = await fetch(`http://${serverIP}.nip.io:81/${clientIpAndPort}`)
       if (res.status === 200) return new Response(null)
       else return new Response(null, { status: 400 })
